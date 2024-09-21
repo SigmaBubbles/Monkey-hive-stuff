d6193529c14275472c0756e6f0e6a86c247ca5ec
SigmaBubbles <happygoodgod223@gmail.com>
2024-09-21 16:55:30 -0500

    Create SigmaRizz.cs

---
+++ SigmaRizz.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class DynamicScript
+{
+    public void Run()
+    {
+        Debug.Log("Dynamic script executed!");
+        // Add any other functionality here.
+    }
+}
